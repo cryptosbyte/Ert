8fb170f4217546d6cc0bafdeea2337ab355f998b
cryptosbyte <74561974+cryptosbyte@users.noreply.github.com>
2021-08-18 13:13:37 +0100

    😔📞 Removing "build/" directory

--- build/src/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const Ert_1 = __importDefault(require("./Ert"));
-const ertConfig = require(process.argv[2]);
-new Ert_1.default(ertConfig).start();
